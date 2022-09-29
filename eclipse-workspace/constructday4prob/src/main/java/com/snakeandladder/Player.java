059ae4f17a9baa2e2b9f236203a9ae7a90b7fd11
shitalbhingewad <shitalbhingewad853@gmail.com>
2022-09-29 12:12:25 +0530

    InitialPlayerPosition

---
+++ eclipse-workspace/constructday4prob/src/main/java/com/snakeandladder/Player.java
@@ -0,0 +1,17 @@
+package com.snakeandladder;
+
+public class Player {
+	private int playerPosition;
+
+	Player() {
+		playerPosition = 0;
+	}
+
+	public int getPlayerPosition() {
+		return playerPosition;
+	}
+
+	public void setPlayerPosition(int playerPosition) {
+		this.playerPosition = playerPosition;
+	}
+}
